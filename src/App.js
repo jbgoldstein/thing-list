2a31560c2306a3ba078f6d43ca6c9903ada0bf33
Jack Goldstein <superuserperson4@gmail.com>
2017-05-24 09:05:12 -0400

    Make Add Thing button work

--- src/App.js
+++ src/App.js
@@ -4,23 +4,47 @@ import ThingList from './ThingList.js'
 import './App.css'
 
 class App extends Component {
-  state = {
-    things: {
-      'thing-1': { id: 'thing-1', name: 'Milk' },
-      'thing-2': { id: 'thing-2', name: 'Bread' },
-      'thing-3': { id: 'thing-3', name: 'Bibb lettuce' },
+  constructor(props) {
+    super(props)
+    this.state = {
+    things: [],
+    max: 0,
+    thing: '',
     }
+
+    this.addThing = this.addThing.bind(this)
+    this.updateThing = this.updateThing.bind(this)
   }
 
-  // addThing() {
-  //   this.setState
-  // }
+  addThing(ev) {
+    const state = {...this.state}
+    const thing = {
+        id: 'thing-' + state.max,
+        name: state.thing,
+    }
+
+    state.things.push(thing)
+    state.max ++
+    state.thing = ''
+    this.setState(state)
+  }
+
+  updateThing(ev) {
+        this.setState ({
+            thing: ev.target.value
+        })
+    }
 
   render() {
     return (
       <div className="App">
         <Header/>
-        <button className="add-thing">Add Thing</button>
+        <textarea className="textBox"
+          value={this.state.thing} 
+          placeholder="Enter thing here"
+          onChange={this.updateThing}>
+        </textarea>
+        <button className="add-thing" onClick={this.addThing}>Add Thing</button>
         <ThingList things={this.state.things} /*addThing={this.addThing}*//>
       </div>
     );
