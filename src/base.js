d801815760f2f20df2eeb135808c116d54821cc1
Jack Goldstein <superuserperson4@gmail.com>
2017-05-25 15:24:04 -0400

    Sign in via GitHub (through firebase)

--- src/base.js
+++ src/base.js
@@ -1,6 +1,7 @@
 import Rebase from 're-base'
 import firebase from 'firebase/app'
 import database from 'firebase/database'
+import 'firebase/auth'
 
 const app = firebase.initializeApp({
     apiKey: "AIzaSyAd2IFSjJxFbUje_jj7P-uyex7luM0Bias",
@@ -12,5 +13,8 @@ const app = firebase.initializeApp({
 })
 
 const db = database(app)
+
+export const auth = app.auth()
+export const githubProvider = new firebase.auth.GithubAuthProvider()
 
 export default Rebase.createClass(db)
