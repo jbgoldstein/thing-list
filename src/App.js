a65f0e728c59315bf0396959b8bbbb8efd187455
Jack Goldstein <superuserperson4@gmail.com>
2017-05-25 14:24:22 -0400

    Add due dates to each thing

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
       id: `thing-${Date.now()}`,
       name: '',
       completed: false,
+      dueOn: null,
     }
   }
 
--- src/Thing.js
+++ src/Thing.js
@@ -10,18 +10,13 @@ class Thing extends Component{
         }
     }
 
-    updateName = (ev) => {
+    handleChange = (ev) => {
         const { thing, saveThing } = this.props
-        thing.name = ev.target.value
+        const field = ev.currentTarget.getAttribute('name')
+        thing[field] = ev.target.value
         saveThing(thing)
     }
-
-    updateCheckbox = (ev) => {
-        const { thing, saveThing } = this.props
-        thing.completed = ev.target.checked
-        saveThing(thing)
-    }
-
+    
     blurOnEnter = (ev) => {
         if (ev.key === 'Enter') {
             ev.preventDefault()
@@ -35,15 +30,23 @@ class Thing extends Component{
             <li className="Thing">
             <input 
             type="checkbox"
-            onChange={this.updateCheckbox} 
+            name="completed"
+            onChange={this.handleChange} 
             defaultChecked={thing.completed}/>
             <div className="details">
                 <ContentEditable
                 className="name"
+                name="name"
                 html={thing.name}
-                onChange={this.updateName}
+                onChange={this.handleChange}
                 onKeyPress={this.blurOnEnter}
                 ref={input => this.nameInput = input}
+                />
+                <input 
+                    type="date"
+                    name="dueOn"
+                    defaultValue={thing.dueOn}
+                    onChange={this.handleChange}
                 />
                 <Actions thing={thing} removeThing={removeThing}/>
             </div>
